d8e5c9c354aba13e130250b761e4d0ad4f22ebf5
Артем Пилипенко <artem38skull@yandex.ru>
2023-01-13 19:00:07 +0800

    Bug fix in list

--- homework.py
+++ homework.py
@@ -296,7 +296,7 @@ def main(training: Training) -> None:
 
 if __name__ == '__main__':
     packages = [
-        ('SWMs', [720, 1, 80, 25, 40]),
+        ('SWM', [720, 1, 80, 25, 40]),
         ('RUN', [15000, 1, 75]),
         ('WLK', [9000, 1, 75, 180]),
     ]
